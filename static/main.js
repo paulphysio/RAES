d2cc0c85517054bcfc24477cc9bbdd9bcadf1294
Mbappe <obinnap350@gmail.com>
2022-10-08 16:57:09 -0700

    second

--- static/main.js
+++ static/main.js
@@ -1,4 +1,12 @@
-
 function file(){
-    document.getElementById(filer).appendChild=`hey`
+    const hide = document.querySelector('.hide-file')
+    const hideBut = document.querySelector('.hide')
+    if (hideBut.innerText==="upload"){
+        hide.classList.remove("not-visible")
+        hideBut.innerText = "Hide"
+    }else{
+        hide.classList.add("not-visible")
+        hideBut.innerText = "upload"
+    }
+   
 }
